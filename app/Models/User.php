16952eecd89ad391e5463ae1f030b0c7a987cb3d
mr-leo44 <lionnelkaniki44@gmail.com>
2025-10-03 23:55:28 +0100

    fix(auth):  add HasRoles and HasApiTokens trait on User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,13 @@ use App\Models\Clan;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
