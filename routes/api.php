863fad3bc9ff7826222bd98fd351785ba2b24197
mr-leo44 <lionnelkaniki44@gmail.com>
2025-10-04 00:14:02 +0100

    feat(auth): add auth routes with throttling

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class,'register'])->middleware('throttle:5,1');
+    Route::post('login', [AuthController::class,'login'])->middleware('throttle:10,1');
+    Route::post('social', [AuthController::class,'socialLogin'])->middleware('throttle:10,1');
+    Route::middleware('auth:sanctum')->post('logout', [AuthController::class,'logout']);
+});
