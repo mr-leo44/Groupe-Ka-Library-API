1a7ef88336a57656f4a81515115598a4a10c2f3b
mr-leo44 <lionnelkaniki44@gmail.com>
2025-10-03 01:30:29 +0100

    feat: update user Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\City;
+use App\Models\Clan;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -45,4 +47,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function city() { return $this->belongsTo(City::class); }
+    public function clan() { return $this->belongsTo(Clan::class); }
 }
